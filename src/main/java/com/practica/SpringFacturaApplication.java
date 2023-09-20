10cad89cb475556a09ea142c3aac7d5e1bab2271
Meyly-SK <meylymerma03@gmail.com>
2023-09-19 19:50:25 -0500

    Creacion Proyecto

---
+++ src/main/java/com/practica/SpringFacturaApplication.java
@@ -0,0 +1,13 @@
+package com.practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFacturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica/SpringFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
